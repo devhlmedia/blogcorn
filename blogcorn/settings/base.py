cbda2aa034ec209e3b11a3e699dd45a68c538879
Sunil Kumar <sunil.kumar@nescode.com>
2016-02-25 20:16:41 +0530

    Post app and model update

--- blogcorn/settings/base.py
+++ blogcorn/settings/base.py
@@ -32,6 +32,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Custom apps
+    'posts',
 )
 
 MIDDLEWARE_CLASSES = (
