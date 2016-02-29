40ab75326ba96462f045fd293ac981dc73a056b7
Sunil Kumar <sunil.kumar@nescode.com>
2016-02-29 19:51:54 +0530

    App urls

--- posts/views.py
+++ posts/views.py
@@ -3,5 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def post_home(request):
+def post_create(request):
     return HttpResponse("<h1>Hello there!</h1>")
+
+def post_detail(request):
+    return HttpResponse("<h1>Hello detail!</h1>")
+
+def post_list(request):
+    return HttpResponse("<h1>Hello list!</h1>")
+
+def post_update(request):
+    return HttpResponse("<h1>Hello update!</h1>")
+
+def post_delete(request):
+    return HttpResponse("<h1>Hello delete!</h1>")
